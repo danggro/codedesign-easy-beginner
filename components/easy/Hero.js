ef08c40bda573f34e646e94fdc703ad2ae40bfb9
danggro <anangdwin55@gmail.com>
2023-03-18 17:33:44 +0700

    error vercel 2

--- components/easy/Hero.js
+++ components/easy/Hero.js
@@ -24,7 +24,7 @@ export default function Hero() {
           height="794"
         />
         <Image
-          src="/rectangle.png"
+          src="/Rectangle.png"
           alt=""
           className="absolute xl:bottom-0 bottom-[220px] left-0 -z-20 xl:left-[140px]"
           width="590"
